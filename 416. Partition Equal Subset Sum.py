57e30dd0f19bf5876e6847406361ac86552e79a9
mh-rahman <rahmanhaseeb32@gmail.com>
2020-05-16 13:51:49 -0700

    Bitwise Solution for 416. Partition Equal Subset Sum.py

--- 416. Partition Equal Subset Sum.py
+++ 416. Partition Equal Subset Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition_knapsack(self, nums: List[int]) -> bool:
         def knapSack(W, wt, val):
             n = len(wt)
             K = [[0 for x in range(W+1)] for x in range(n+1)] 
@@ -24,3 +24,29 @@ class Solution:
             return True           
         
         return False
+        
+ 
+ 
+    def canPartition_bitwise(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        
+        creating a dp array that will have True at all positions which can be acheived as sum of any combination of numbers
+        """
+        sum_val = sum(nums)
+        if sum_val % 2 == 1:
+            return False
+        target = sum_val // 2
+        dp = [False] * (sum_val + 1)
+        dp[0] = True
+        for num in nums:
+            next_dp = [False] * (sum_val + 1)
+            for j in xrange(len(dp)):
+                if dp[j]:
+                    next_dp[j + num] = True
+                    next_dp[j] = True
+            dp = next_dp
+            if dp[target]:
+                return dp[target]
+        return dp[target]

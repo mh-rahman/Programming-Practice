4f105ac4f8069817380fa635aeec9d940166e210
mh-rahman <rahmanhaseeb32@gmail.com>
2020-05-18 22:20:54 -0700

    581. Shortest Unsorted Continuous Subarray

--- 581. Shortest Unsorted Continuous Subarray.py
+++ 581. Shortest Unsorted Continuous Subarray.py
@@ -33,44 +33,29 @@ class Solution:
         #     i+=1
             
         i = 0
+        j = l-2
+        start = l-1
+        min_ = nums[start]
         end = 0
         max_ = nums[end]
         i+=1
         safe_max = -math.inf
-        while i<l:
+        safe_min = math.inf
+        while i<l and j>=0:
             if nums[i]>=nums[i-1] and nums[i]>=max_:
-                # print(i,nums[i],'safe')
-                safe_max = max(safe_max, nums[i])
-                # i+=1
+                safe_max = nums[i]
             else:
                 end = i
                 max_ = max(max_,nums[i],safe_max)
                 safe_max = -math.inf
-                # i+=1
-            # print(i,nums[i],'max = ', max_, ' safe_max = ', safe_max)
-            i+=1
-        # print('End = ', end,'i.e.', nums[end])
-            
-        # print('\n\n\n')
-        i = l-1
-        start = l-1
-        min_ = nums[start]
-        i-=1
-        safe_min = math.inf
-        while i>=0:
-            if nums[i]<=nums[i+1] and nums[i]<=min_:
-                # print(i,nums[i],'safe')
-                safe_min = min(safe_min, nums[i])
-                # i+=1
+            if nums[j]<=nums[j+1] and nums[j]<=min_:
+                safe_min = nums[j]
             else:
-                start = i
-                min_ = min(min_,nums[i],safe_min)
+                start = j
+                min_ = min(min_,nums[j],safe_min)
                 safe_min = math.inf
-                # i+=1
-            # print(i,nums[i],'min = ', min_, ' safe_min = ', safe_min)
-            i-=1
-        # print('start = ', start,'i.e.', nums[start])
             
-            
-            
+            i+=1
+            j-=1
+
         return max(end-start+1,0)

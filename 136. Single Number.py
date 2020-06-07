2d2b13ad1bbe3a1175a31ebaea0cc60322b49ab2
mh-rahman <rahmanhaseeb32@gmail.com>
2020-06-06 23:42:27 -0700

    136. Single Number

---
+++ 136. Single Number.py
@@ -0,0 +1,7 @@
+# from operator import xor
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for n in nums:
+            res = res^n
+        return res

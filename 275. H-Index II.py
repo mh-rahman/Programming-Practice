c0f2e08153ec9ea29e98a829c95a09e8e776cbd8
mh-rahman <rahmanhaseeb32@gmail.com>
2020-06-18 07:52:54 -0700

    log(N) approach for 275. H-Index II.py

--- 275. H-Index II.py
+++ 275. H-Index II.py
@@ -1,12 +1,39 @@
 class Solution:
     def hIndex(self, c: List[int]) -> int:
         
+        #One liner
         return max([min(len(c)-i,n) for i,n in enumerate(c)]) if c else 0
-
-#         rank = 0
+        
+        ##Linear time approach
 #         l = len(c)
 #         for i in range(l):
+#             # print(i)
 #             temprank = min(l-i,c[i])
-#             rank = max(rank,temprank)
+#             if temprank < rank:
+#                 break
+#             else:
+#                 rank = temprank
             
 #         return rank
+
+
+    ##Attempted log(N) approach
+#         rank = 0
+#         l = len(c)
+#         li = 0
+#         ri = l-1
+        
+#         while li<=ri:
+#             i = (li+ri)//2
+#             temprank = min(l-i,c[i])
+#             print(li,ri,i,temprank,rank)
+#             if rank and temprank <= rank:
+#                 ri = i-1
+#             else:
+#                 li = i+1
+#             rank = max(rank,temprank)
+#         return max(rank,0)
+        
+        
+        
+        
